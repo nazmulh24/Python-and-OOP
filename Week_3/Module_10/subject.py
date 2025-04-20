d8b2c3b95994030308f640a545b7a94a4cf5df22
Nazmul Hossain <nazmulhossain@Nazmuls-MacBook-Air.local>
2025-04-20 10:38:53 +0600

    Complete Subject class

--- Week_3/Module_10/subject.py
+++ Week_3/Module_10/subject.py
@@ -1,15 +1,15 @@
-from person import Person
+from person import Teacher
 from school import School
 
 
 class Subject:
     def __init__(self, name, teacher):
         self.name = name
-        self.teacher = teacher
+        self.teacher = teacher  # --> teacher er object thakbe
         self.max_marks = 100
         self.pass_marks = 40
 
-    def exam(self, students):
+    def exam(self, students):  # --> student er list
         for student in students:
             mark = self.teacher.evaluate_exam()
             student.marks[self.name] = mark
